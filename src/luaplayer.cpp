6fa393cc2a5f4f0917496fe0ff53411a84159bcd
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-12-26 00:06:43 -0300

    add panic funcion
    depends on https://github.com/ps2dev/lua/pull/6 to be able to fix the issues with `longjmp()`

--- src/luaplayer.cpp
+++ src/luaplayer.cpp
@@ -4,9 +4,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <malloc.h>
+#include <debug.h>
 
 #include "include/luaplayer.h"
 
+#ifndef NO_PANIC_FUNCTION
+int test_error(lua_State * L);
+#endif
 static lua_State *L;
 
 const char * runScript(const char* script, bool isStringBuffer )
@@ -14,7 +18,9 @@ const char * runScript(const char* script, bool isStringBuffer )
     printf("Creating luaVM... \n");
 
   	L = luaL_newstate();
-	
+#ifndef NO_PANIC_FUNCTION
+    lua_atpanic(L, test_error);
+#endif
 	  // Init Standard libraries
 	  luaL_openlibs(L);
 
@@ -58,3 +64,58 @@ const char * runScript(const char* script, bool isStringBuffer )
 	
 	return errMsg;
 }
+#define TPRINTF(arg, x...) \
+    printf(arg, ##x); \
+    scr_printf(arg, ##x)
+
+#ifndef NO_PANIC_FUNCTION
+int test_error(lua_State * L) {
+    scr_clear();
+    init_scr();
+    scr_clear();
+    scr_clear();
+    scr_setCursor(0);
+    int n = lua_gettop(L);
+    int i;
+    scr_printf("\t");
+    TPRINTF("LUA ERROR.\n");
+
+    if (n == 0) {
+        scr_printf("\t");
+        TPRINTF("Stack is empty!!!!\n");
+    }
+
+    for (i = 1; i <= n; i++) {
+        scr_printf("\t");
+        TPRINTF("%i: ", i);
+        switch(lua_type(L, i)) {
+        case LUA_TNONE:
+            TPRINTF("Invalid");
+            break;
+        case LUA_TNIL:
+            TPRINTF("(Nil)");
+            break;
+        case LUA_TNUMBER:
+            TPRINTF("(Number) %f", lua_tonumber(L, i));
+            break;
+        case LUA_TBOOLEAN:
+            TPRINTF("(Bool)   %s", (lua_toboolean(L, i) ? "true" : "false"));
+            break;
+        case LUA_TSTRING:
+            TPRINTF("%s", lua_tostring(L, i));
+            break;
+        case LUA_TTABLE:
+            TPRINTF("(Table)");
+            break;
+        case LUA_TFUNCTION:
+            TPRINTF("(Function)");
+            break;
+        default:
+            TPRINTF("Unknown");
+        }
+    TPRINTF("\n");
+    }
+    SleepThread(); //else, crash still ocurs
+    return 0;
+}
+#endif
