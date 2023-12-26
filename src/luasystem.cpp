d50cf754a3be08861d99438e72fc27c99c7aa3fa
israpps <57065102+israpps@users.noreply.github.com>
2023-12-26 16:35:32 -0300

    dont stat on `System.listDirectory()`

--- src/luasystem.cpp
+++ src/luasystem.cpp
@@ -186,10 +186,6 @@ static int lua_dir(lua_State *L)
 			lua_pushstring(L, "name");
         	lua_pushstring(L, dir->d_name);
         	lua_settable(L, -3);
-        		
-        	lua_pushstring(L, "size");
-        	lua_pushnumber(L, dir->d_stat.st_size);
-        	lua_settable(L, -3);
         	lua_pushstring(L, "directory");
         	lua_pushboolean(L, (dir->d_type == DT_DIR));
         	lua_settable(L, -3);
