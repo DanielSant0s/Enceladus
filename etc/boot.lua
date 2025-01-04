5462addcb43343ba41302d25c1cc3cf717171ba0
Matías Israelson <57065102+israpps@users.noreply.github.com>
2025-01-04 11:17:38 -0300

    [builtin sript]: inform end of execution instead of blank error

--- etc/boot.lua
+++ etc/boot.lua
@@ -1,14 +1,15 @@
-
 if doesFileExist("System/index.lua") then
-	dofile("System/index.lua");
+    dofile("System/index.lua");
   elseif doesFileExist("System/script.lua") then
-	dofile("System/script.lua");
+    dofile("System/script.lua");
   elseif doesFileExist("System/system.lua") then
-	dofile("System/system.lua");
+    dofile("System/system.lua");
   elseif doesFileExist("index.lua") then
-	dofile("index.lua");
+    dofile("index.lua");
   elseif doesFileExist("script.lua") then
-	dofile("script.lua");
+    dofile("script.lua");
   elseif doesFileExist("system.lua") then
-	dofile("system.lua");
+    dofile("system.lua");
 end
+
+error("end of script reached");
