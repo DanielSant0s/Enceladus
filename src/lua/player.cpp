1abcd304deb891cdfa6ea9a8fbec41cf130471e2
Matías Israelson <57065102+israpps@users.noreply.github.com>
2025-10-30 17:08:48 -0300

    print lua stack on error screen

--- src/lua/player.cpp
+++ src/lua/player.cpp
@@ -10,11 +10,11 @@
 static lua_State *L;
 
 const char * runScript(const char* script, bool isStringBuffer )
-{	
+{
     printf("Creating luaVM... \n");
 
   	L = luaL_newstate();
-	
+
 	  // Init Standard libraries
 	  luaL_openlibs(L);
 
@@ -33,28 +33,70 @@ const char * runScript(const char* script, bool isStringBuffer )
 #endif
 
     printf("done !\n");
-     
+
 	if(!isStringBuffer){
         printf("Loading script : `%s'\n", script);
 	}
 
-	int s = 0;
-	const char * errMsg =(const char*)malloc(sizeof(char)*512);
+	int s = 0, l = 0;
+	char * errMsg =(char*)malloc(sizeof(char)*512);
 
 	if(!isStringBuffer) s = luaL_loadfile(L, script);
 	else {
     s = luaL_loadbuffer(L, script, strlen(script), NULL);
   }
 
-		
+
 	if (s == 0) s = lua_pcall(L, 0, LUA_MULTRET, 0);
 
 	if (s) {
-		sprintf((char*)errMsg, "%s\n", lua_tostring(L, -1));
-    printf("%s\n", lua_tostring(L, -1));
+		l = sprintf(&errMsg[l], "\t%s\n", lua_tostring(L, -1));
+    	printf("%s\n", lua_tostring(L, -1));
+#ifndef LUAERROR_DONT_PRINT_STACK
+    	int n = lua_gettop(L);
+    	int i;
+
+    	if (n == 0) {
+			l += sprintf(&errMsg[l], "Stack is empty.\n");
+			return 0;
+    	}
+
+    	for (i = 1; i <= n; i++) {
+			l += sprintf(&errMsg[l], "\tLUA Stack:\n");
+			l += sprintf(&errMsg[l], "\t[%i]: ", i);
+    		switch(lua_type(L, i)) {
+    		    case LUA_TNONE:
+			    	l += sprintf(&errMsg[l], "Invalid");
+    		        break;
+    		    case LUA_TNIL:
+    		        l += sprintf(&errMsg[l], "(Nil)");
+    		        break;
+    		    case LUA_TNUMBER:
+    		        l += sprintf(&errMsg[l], "(Number) %f", lua_tonumber(L, i));
+    		        break;
+    		    case LUA_TBOOLEAN:
+    		        l += sprintf(&errMsg[l], "(Bool)   %s", (lua_toboolean(L, i) ? "true" : "false"));
+    		        break;
+    		    case LUA_TSTRING:
+    		        l += sprintf(&errMsg[l], "(String) %s", lua_tostring(L, i));
+    		        break;
+    		    case LUA_TTABLE:
+    		        l += sprintf(&errMsg[l], "(Table)");
+    		        break;
+    		    case LUA_TFUNCTION:
+    		        l += sprintf(&errMsg[l], "(Function)");
+    		        break;
+    		    default:
+    		        l += sprintf(&errMsg[l], "Unknown");
+			}
+
+			l += sprintf(&errMsg[l], "\n");
+    	}
+#endif
+
 		lua_pop(L, 1); // remove error message
 	}
 	lua_close(L);
-	
-	return errMsg;
+
+	return (const char*)errMsg;
 }
